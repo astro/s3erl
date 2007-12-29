daf55f033cb4b20bbb94b0638ae10f4508fc6756
andy@nobugs.org <andy@nobugs.org>
2007-12-29 22:58:40 +0000

    Initial released version

---
+++ src/s3test.erl
@@ -0,0 +1,20 @@
+-module(s3test).
+-export([run/3]).
+-include("s3.hrl").
+ 
+run( AKI, SAK, Bucket ) ->
+
+    Credentials = #aws_credentials{ accessKeyId=AKI, secretAccessKey=SAK },
+    Key = "testkey",
+
+    {ok, _} = s3:start( Credentials ),
+
+    io:format("Exercising s3erl on bucket ~p~n", [Bucket]),
+    io:format("Listing all buckets: ~p~n", [ s3:list_buckets() ] ),
+    io:format("Creating new bucket: ~p~n", [ s3:create_bucket( Bucket ) ] ),
+
+    io:format("Writing object ~p~n", [s3:write_object( Bucket, Key, <<1,2,3>>, "text/plain")] ),
+    io:format("Listing objects ~p~n", [s3:list_objects( Bucket )] ),
+    io:format("Deleting object ~p~n", [s3:delete_object( Bucket, Key )] ),
+    io:format("Deleting bucket ~p~n", [s3:delete_bucket( Bucket )] ),
+    io:format("Test completed~n", []).
