a0c045375f217275ecca70f2a329753917db3634
Astro <astro@spaceboyz.net>
2010-04-27 02:13:51 +0200

    s3test: check reading

--- src/s3test.erl
+++ src/s3test.erl
@@ -14,7 +14,10 @@ run( AKI, SAK, Bucket ) ->
     io:format("Listing all buckets: ~p~n", [ s3:list_buckets() ] ),
     io:format("Creating new bucket: ~p~n", [ s3:create_bucket( Bucket ) ] ),
 
-    io:format("Writing object ~p~n", [s3:write_object( Bucket, Key, <<1,2,3>>, "text/plain")] ),
+    Write = <<1, 2, 3>>,
+    io:format("Writing object ~p~n", [s3:write_object( Bucket, Key, Write, "text/plain")] ),
+    {_, Read} = s3:read_object( Bucket, Key ),
+    io:format("Reading object, match? ~p~n", [list_to_binary(Read) == Write] ),
     io:format("Listing objects ~p~n", [s3:list_objects( Bucket )] ),
     io:format("Deleting object ~p~n", [s3:delete_object( Bucket, Key )] ),
     io:format("Deleting bucket ~p~n", [s3:delete_bucket( Bucket )] ),
